9f3855a6e427446e4a482da902bbcb381fd744de
Alfredo Suarez <asuarez4704@gmail.com>
2022-01-21 21:17:59 -0500

    migrate to jupyter

--- model.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-
-training_set = pd.read_csv('Datasets/twitter_training.csv')
-message_content = training_set[["Tweet Content"]]
-sentiment = training_set[["Sentiment"]]
-
