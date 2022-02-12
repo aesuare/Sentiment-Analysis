4da24b0783e6ee746671621d075273106ba38787
Alfredo Suarez <asuarez4704@gmail.com>
2022-02-12 14:33:43 -0500

    swapped encoding type for better performance

--- Datasets/Texts/TextstoExcel.py
+++ Datasets/Texts/TextstoExcel.py
@@ -1,5 +1,6 @@
 import os
 for entry in os.scandir('.'):
     if entry.is_file():
-        with open(entry, 'rb') as curr_file:
+        # with open(entry, 'rb') as curr_file:
+        with open(entry, encoding ='ISO-8859-1') as curr_file:
             print(curr_file.read())
