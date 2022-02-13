69fc9ad79ad65ca445898ed81b21cbfc88575935
Alfredo Suarez <asuarez4704@gmail.com>
2022-02-13 15:21:14 -0500

    change encoding type

--- Datasets/Texts/TextstoExcel.py
+++ Datasets/Texts/TextstoExcel.py
@@ -1,6 +1,21 @@
 import os
+from tkinter.tix import TEXT
+
+
+TEXT_FILES = []
+
+
 for entry in os.scandir('.'):
     if entry.is_file():
-        # with open(entry, 'rb') as curr_file:
-        with open(entry, encoding ='ISO-8859-1') as curr_file:
-            print(curr_file.read())
+        with open(entry, encoding='iso-8859-1') as curr_file:
+        # with open(entry, encoding ='ISO-8859-1') as curr_file:
+            file_contents = curr_file.read()
+            # print(file_contents)
+            TEXT_FILES.append(file_contents)
+            print("====================================================================================")
+
+
+
+
+print(TEXT_FILES)
+            
