f2aac71668691b649a3c02309f2fb76ed25cb49e
Alfredo Suarez <asuarez4704@gmail.com>
2022-02-23 12:45:20 -0500

    instantiate file to call readile function

---
+++ readfiles.py
@@ -0,0 +1,34 @@
+import os
+from WordIndex import word_index
+
+
+TEXT_FILES = []
+
+
+for entry in os.scandir('.'):
+    if entry.is_file():
+        with open(entry, encoding='iso-8859-1') as curr_file:
+        # with open(entry, encoding ='ISO-8859-1') as curr_file:
+            file_contents = curr_file.read()
+            clean_contents = []
+
+            list_of_words = file_contents.split()
+
+            for word in list_of_words:
+                is_word_alpha = word.isalpha()
+                if is_word_alpha:
+                    clean_contents.append(word.lower())
+
+            paragraph = " ".join(clean_contents)
+            TEXT_FILES.append(paragraph)
+
+
+for paragraph in TEXT_FILES:
+    int_words = []
+    for word in paragraph:
+        int_word = word_index[word]
+        if int_word:
+            int_words.append(int_word)
+        else:
+            int_words.append(1)
+    print(int_words)
